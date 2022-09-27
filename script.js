7a2b201fe3c6ca35ee55289069a75abe7d676e6d
EduNishimura <edunish@gmail.com>
2022-09-26 21:19:32 -0300

    Second page concept

---
+++ script.js
@@ -0,0 +1,10 @@
+function Page01() {   
+
+    const body01 = document.getElementById("body");
+    body01.style.backgroundImage = 'url(Images/Page01.png)';
+
+    document.getElementById("page00").style.display = 'none';
+    document.getElementById("page01").style.display = 'flex';
+    document.getElementById("nav00").style.marginBottom = '2em';
+
+}
